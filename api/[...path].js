e13e51cefcbbc54a60e3a11f4688932dce5dfa96
Ujwal Magar <magarujal6@gmail.com>
2026-03-21 15:45:10 +0545

    fix: add missing Vercel serverless entry point api/[...path].ts

--- api/[...path].js
+++ api/[...path].js
@@ -1,19 +1,2 @@
-let app;
-
-try {
-  app = require("../backend/dist/app").default;
-} catch (err) {
-  // If the require fails, return a helpful error
-  module.exports = (req, res) => {
-    res.status(500).json({
-      error: "Function initialization failed",
-      details: err.message,
-    });
-  };
-}
-
-if (app) {
-  module.exports = (req, res) => {
-    return app(req, res);
-  };
-}
+import app from "../src/app.js";
+export default app;
